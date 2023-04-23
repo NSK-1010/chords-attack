cf48097640922a19f088a71bdccd8c4fd86d1a42
猫宮ぴあの / Piano Nekomiya <kotone.olin1010@gmail.com>
2023-04-23 20:49:51 +0900

    いらないコードがあった

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,10 +4,10 @@
 )]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
+/* #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
-}
+} */
 
 fn main() {
     tauri::Builder::default()
